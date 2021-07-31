5f81fbc11aba10861278bfb4074231917f67e4cf
Di Guan <dguan@purdue.edu>
2021-07-31 19:21:08 +0800

    first commit

---
+++ src/main/java/com/ssm/syn/AdminController.java
@@ -0,0 +1,11 @@
+package com.ssm.syn;
+
+/**
+ * @author Di Guan on 7/31/2021 7:19 PM
+ */
+
+public class AdminController {
+    public static void main(String[] args) {
+        System.out.println("first test");
+    }
+}
